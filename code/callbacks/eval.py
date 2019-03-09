5a4bdd1da4d32f1c4622f4e845ac01c358633aaf
Martin Kayser <makayser@microsoft.com>
2019-03-09 13:06:33 +0100

    notebooks with amls

--- code/callbacks/eval.py
+++ code/callbacks/eval.py
@@ -1,12 +1,10 @@
+
 """
 Copyright 2017-2018 Fizyr (https://fizyr.com)
-
 Licensed under the Apache License, Version 2.0 (the "License");
 you may not use this file except in compliance with the License.
 You may obtain a copy of the License at
-
     http://www.apache.org/licenses/LICENSE-2.0
-
 Unless required by applicable law or agreed to in writing, software
 distributed under the License is distributed on an "AS IS" BASIS,
 WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -15,7 +13,7 @@ limitations under the License.
 """
 
 import keras
-from ..utils.eval import evaluate
+#from ..utils.eval import evaluate
 
 
 class Evaluate(keras.callbacks.Callback):
@@ -25,16 +23,18 @@ class Evaluate(keras.callbacks.Callback):
     def __init__(
         self,
         generator,
-        iou_threshold=0.5,
-        score_threshold=0.05,
-        max_detections=100,
+        ## can these be changed via arguments?
+        ## changed 
+        iou_threshold=0.25,
+        score_threshold=0.15,
+        ## changed
+        max_detections=500,
         save_path=None,
         tensorboard=None,
         weighted_average=False,
         verbose=1
     ):
         """ Evaluate a given dataset using a given model at the end of every epoch during training.
-
         # Arguments
             generator        : The generator that represents the dataset to evaluate.
             iou_threshold    : The threshold used to consider when a detection is positive or negative.
@@ -60,7 +60,7 @@ class Evaluate(keras.callbacks.Callback):
         logs = logs or {}
 
         # run evaluation
-        average_precisions = evaluate(
+        false_positives_dict, true_positives_dict, average_precisions, iou, image_names, detection_list, scores_list, labels_list = evaluate(
             self.generator,
             self.model,
             iou_threshold=self.iou_threshold,
@@ -83,15 +83,88 @@ class Evaluate(keras.callbacks.Callback):
         else:
             self.mean_ap = sum(precisions) / sum(x > 0 for x in total_instances)
 
+        #print(precisions)
+        ## i think here tensorboard file is written
         if self.tensorboard is not None and self.tensorboard.writer is not None:
             import tensorflow as tf
             summary = tf.Summary()
+            
             summary_value = summary.value.add()
             summary_value.simple_value = self.mean_ap
             summary_value.tag = "mAP"
             self.tensorboard.writer.add_summary(summary, epoch)
+            
+            self.mIoU = np.mean(iou)
+            summary_value = summary.value.add()
+            summary_value.simple_value = self.mIoU
+            summary_value.tag = "mIoU"
+            self.tensorboard.writer.add_summary(summary, epoch)
+            
+            self.EAD_Score = 0.8*self.mean_ap + 0.2*self.mIoU
+            summary_value = summary.value.add()
+            summary_value.simple_value = self.EAD_Score
+            summary_value.tag = "EAD Score"
+            self.tensorboard.writer.add_summary(summary, epoch)    
+            
+            self.AP1 = precisions[0]
+            total_instances.append(num_annotations)
+            summary_value = summary.value.add()
+            summary_value.simple_value = self.AP1
+            summary_value.tag = "specularity mAP"
+            self.tensorboard.writer.add_summary(summary, epoch)
+            
+            self.AP2 = precisions[1]
+            summary_value = summary.value.add()
+            summary_value.simple_value = self.AP2
+            summary_value.tag = "saturation mAP"
+            self.tensorboard.writer.add_summary(summary, epoch)
+            
+            self.AP3 = precisions[2]
+            summary_value = summary.value.add()
+            summary_value.simple_value = self.AP3
+            summary_value.tag = "artifact mAP"
+            self.tensorboard.writer.add_summary(summary, epoch)
+            
+            self.AP4 = precisions[3]
+            summary_value = summary.value.add()
+            summary_value.simple_value = self.AP4
+            summary_value.tag = "blur mAP"
+            self.tensorboard.writer.add_summary(summary, epoch)            
 
+            self.AP5 = precisions[4]
+            summary_value = summary.value.add()
+            summary_value.simple_value = self.AP5
+            summary_value.tag = "contrast mAP"
+            self.tensorboard.writer.add_summary(summary, epoch) 
+            
+            self.AP6 = precisions[5]
+            summary_value = summary.value.add()
+            summary_value.simple_value = self.AP6
+            summary_value.tag = "bubbles mAP"
+            self.tensorboard.writer.add_summary(summary, epoch) 
+            
+            self.AP7 = precisions[6]
+            summary_value = summary.value.add()
+            summary_value.simple_value = self.AP7
+            summary_value.tag = "instrument mAP"
+            self.tensorboard.writer.add_summary(summary, epoch) 
+            
         logs['mAP'] = self.mean_ap
+        logs["mIoU"] = self.mIoU
+        logs["EAD_Score"] = self.EAD_Score
+        logs["specularity mAP"] = self.AP1
+        logs["saturation mAP"] = self.AP2
+        logs["artifact mAP"] = self.AP3
+        logs["blur mAP"] = self.AP4
+        logs["contrast mAP"] = self.AP5
+        logs["bubbles mAP"] = self.AP6
+        logs["instrument mAP"] = self.AP7
+        
+        ##
+        
 
         if self.verbose == 1:
+            #print("Gamma, alpha: ", )
             print('mAP: {:.4f}'.format(self.mean_ap))
+            print('mIoU: {:.4f}'.format(self.mIoU))
+            print('EAD Score: {:.4f}'.format(self.EAD_Score))
