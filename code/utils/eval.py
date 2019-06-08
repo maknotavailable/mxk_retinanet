b63cc490cfd5467bfc9ef112fcf95bbca83fef1d
Maxime Kayser <kayser.mg@gmail.com>
2019-06-08 17:30:28 +0200

    typo

--- code/utils/eval.py
+++ code/utils/eval.py
@@ -11,8 +11,7 @@ See the License for the specific language governing permissions and
 limitations under the License.
 """
 
-from utils.
-compute_overlap import compute_overlap
+from utils.compute_overlap import compute_overlap
 from utils.visualization import draw_detections, draw_annotations
 
 import keras
