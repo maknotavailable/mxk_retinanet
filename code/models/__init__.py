a37729af8612b2487c43ef1890fdb3267616a04a
Maxime Kayser <kayser.mg@gmail.com>
2019-07-07 13:28:27 +0200

    t

--- code/models/__init__.py
+++ code/models/__init__.py
@@ -17,7 +17,7 @@ class Backbone(object):
             'FilterDetections' : layers.FilterDetections,
             'Anchors'          : layers.Anchors,
             'ClipBoxes'        : layers.ClipBoxes,
-            '_smooth_l1'       : losses.(),
+            '_smooth_l1'       : losses.smooth_l1(),
             '_focal'           : losses.focal(),
             '_focal2'          : losses.focal2()
         }
